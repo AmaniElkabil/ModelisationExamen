61714147588cc7bfc1fe5ecb591d62f52f57af9b
AmaniElkabil <amani.elkabil@usmba.ac.ma>
2022-12-02 15:37:14 +0100

    modifier

---
+++ ModélisationExamen/Properties/QCMquestion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    internal class QCMquestion : Question
+    {
+        protected string question;
+        protected int id;
+        protected string choix; 
+
+        public QCMquestion(string question , int id , string choix) 
+        {
+            this.question = question;
+            this.id = id;
+            this.choix = choix; 
+        }
+        public Addchix (bool valeur)
+        {
+        }
+    }
+}
